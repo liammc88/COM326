e5e8ce0e9267be9f2136e27e94d88d1bd5f9e687
liammc88 <43850304+liammc88@users.noreply.github.com>
2018-11-20 15:01:34 +0000

    Create Main.cpp

---
+++ Week 9/Main.cpp
@@ -0,0 +1,13 @@
+//Week 9
+//Challenge 1
+//Description: multi class hierarchy in RPG game
+//Author: Liam McCormick
+//Date: 20/11/2018
+
+#include <iostream>
+#include "Item.h"
+using namespace std;
+
+int main() {
+	Item sword{"Sword", 5, 3.0f};
+}
