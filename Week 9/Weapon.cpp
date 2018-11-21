32587795e372aa7d04bba76f53e54cf4132471ac
liammc88 <43850304+liammc88@users.noreply.github.com>
2018-11-21 16:10:48 +0000

    Update Weapon.cpp

--- Week 9/Weapon.cpp
+++ Week 9/Weapon.cpp
@@ -17,8 +17,8 @@ Weapon::Weapon(): Item("Sword") {
 	std::cout << "Derived class Weapon contructor called" << std::endl;
 }
 
-Weapon::Weapon(std::string weaponName, int value, float weight, int hitStrength, int health)
-	: Item{ weaponName, value, weight }, weaponHitStrength_{ hitStrength }, weaponHealth_{ health } {
+Weapon::Weapon(std::string weaponName, int value, float weight, int hitStrength, int weaponHealth)
+	: Item{ weaponName, value, weight }, weaponHitStrength_{ weaponHitStrength }, weaponHealth_{ weaponHealth } {
 	std::cout << "Constructor for Weapon 1" << std::endl;
 }
 
@@ -27,15 +27,42 @@ void Weapon::SetWeaponName(std::string weaponName) {		//question this?????
 	weaponName = weaponName;
 }
 
-//std::string Weapon::SetWeaponName() {
-//	return weaponName;
-//}
-
-void Weapon::SetWeaponStrength(int hitStrength) {
-	weaponHitStrength_ = hitStrength;
+std::string Weapon::GetWeaponName() {
+	return weaponName;
 }
+
+void Weapon::SetWeaponHitStrength(int weaponHitStrength) {
+	weaponHitStrength_ = weaponHitStrength;
+}
+
+int Weapon::GetWeaponHitStrength() {
+	return weapinHitStrength;
+}
+
+void Weapon::SetWeaponStrength(int health) {
+	weaponHealth_ = weaponHealth;
+}
+
+int Weapon::GetWeaponStrength() {
+	return weaponHealth;
+}
+
+//ToString();
+
+std::string Weapon::ToString() {
+	std::string weaponNameString = GetItemName();
+	std::string weaponValueString = std::to_string(GetItemValue());
+	std::string weaponHitStrengthString = std::to_string(GetWeaponHitStrength());
+	std::string weaponHealthString = std::to_string(GetWeaponHealth());
+	//std::string weight = std::to_string(GetWeight());
+
+	return weaponString_ = "Weapon: " + weaponNameString + "Worth: " + weaponValueString + "Hit strength: " 
+							+ weaponHitStrengthString + "Weapon health: " + weaponHealthString;
+}
+
 
 //void Weapon(std::string weaponName, int value, float weight, int hitStrength, int health)
 //	: Item(weaponName), value_{ value }, weight_{ weight }, hitStrength_{ hitStrength }, health_{ health } {
 //	//std::cout << "Default constructor for Weapon" << std::endl;
 //}
+
